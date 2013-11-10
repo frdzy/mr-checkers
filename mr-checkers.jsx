a437075449aa968dce03bd2316962965b777d031
Fred Zhao <fred.y.zhao@gmail.com>
2013-11-09 17:42:24 -0800

    Added UI to select an existing game or create a new one

--- mr-checkers.jsx
+++ mr-checkers.jsx
@@ -21,6 +21,325 @@ var CheckersLevels = {
   KING: 1
 };
 
+var CheckersApp = React.createClass({
+
+  mixins: [MeteorMixin],
+
+  getMeteorState: function() {
+    var games = CheckersGames.find().fetch();
+    var curGameID = Session.get('current_game_id');
+    var curGame = null;
+    if (curGameID) {
+      games.forEach(function(game) {
+        if (game._id === curGameID) {
+          curGame = game;
+          return false;
+        }
+      });
+    }
+
+    return {
+      currentGameID: curGameID,
+      currentGame: _.extend(
+        {
+          height: 8,
+          width: 8,
+          initialized: !!curGame,
+        },
+        curGame
+      ),
+      games: games
+    };
+  },
+
+  onSelectGame: function(gameID) {
+    Session.set('current_game_id', gameID);
+  },
+
+  render: function() {
+    return (
+      <div className="checkersApp">
+        <CheckersGamesList
+          onSelectGame={this.onSelectGame}
+          currentGameID={this.state.currentGameID}
+          games={this.state.games}
+        />
+        <CheckersStage
+          gameData={this.state.currentGame}
+        />
+      </div>
+    );
+  }
+});
+
+var CheckersGamesList = React.createClass({
+
+  onCreateNewGame: function(evt) {
+    Meteor.call(
+      'createGame',
+      'MyGame',
+      function(error, newGameID) {
+        if (error) {
+          console.error('Failed to create game: ' + error.reason);
+          return;
+        }
+        this.props.onSelectGame(newGameID);
+      }.bind(this)
+    );
+  },
+
+  onSelectGame: function(evt) {
+    this.props.onSelectGame(evt.target.value);
+  },
+
+  render: function() {
+    var gameList = [];
+    this.props.games.forEach(function(game) {
+      var gameKey = "checkersCurrentGame." + game._id;
+      gameList.push(
+        <li key={gameKey}>
+          <input
+            checked={game._id === this.props.currentGameID}
+            onChange={this.onSelectGame}
+            name="checkersCurrentGame"
+            type="radio"
+            value={game._id}
+          />
+          {game.name}
+        </li>
+      );
+    }.bind(this));
+
+    return (
+      <div className="checkersGamesPanel">
+        All Games:
+        <ul className="checkersGamesList">
+          {gameList}
+          <li>
+            <input
+              onClick={this.onCreateNewGame}
+              type="submit"
+              value="Create New Game"
+            />
+          </li>
+        </ul>
+      </div>
+    );
+  }
+});
+
+CheckersGames = new Meteor.Collection("checkers_games");
+
+var CheckersStage = React.createClass({
+
+  render: function() {
+    return (
+      <div className="checkersStage">
+        <CheckersBoard
+          data={this.props.gameData}
+        />
+      </div>
+    );
+  }
+});
+
+var CheckersBoard = React.createClass({
+
+  getPieceAtLocation: function(r, c) {
+    var row, pieceData;
+    var pieces = this.props.data.pieces;
+    if (!pieces) {
+      return null;
+    }
+    if (row = pieces[r]) {
+      if (pieceData = row[c]) {
+        return (
+          <CheckersPiece
+            player={pieceData.player}
+            level={pieceData.level}
+          />
+        );
+      }
+    }
+    return null;
+  },
+
+  getInitializingMessage: function() {
+    if (!this.props.data.initialized) {
+      return (
+        <div className="checkersLoading">
+          Select a Game
+        </div>
+      );
+    }
+  },
+
+  render: function() {
+    var data = this.props.data;
+    var width = data.width;
+    var height = data.height;
+    var rows = [];
+    var parity = true;
+    for (var i = 0; i < height; i++) {
+      var checkersCells = [];
+      for (var j = 0; j < width; j++) {
+        checkersCells.push(
+          <CheckersCell
+            key={'cell.' + i + '.' + j}
+            piece={this.getPieceAtLocation(i, j)}
+            parity={(parity = !parity)}
+          />
+        );
+      }
+      parity = !parity;
+      rows.push(
+        <CheckersRow
+          key={'row.' + i}
+          cells={checkersCells}
+        />
+      );
+    }
+    return (
+      <div className="checkersBoard">
+        <table>
+          <tbody>
+            {rows}
+          </tbody>
+        </table>
+        {this.getInitializingMessage()}
+      </div>
+    );
+  }
+});
+
+var CheckersRow = React.createClass({
+
+  render: function() {
+    return (
+      <tr className="checkersRow">
+        {this.props.cells}
+      </tr>
+    );
+  }
+});
+
+var CheckersCell = React.createClass({
+
+  render: function() {
+    var classNameSuffix = this.props.parity
+      ? "odd"
+      : "even";
+    return (
+      <td className={['checkersCell', classNameSuffix].join(' ')}>
+        {this.props.piece}
+      </td>
+    );
+  }
+});
+
+var CheckersPiece = React.createClass({
+
+  getBorderColorCode: function() {
+    var checkersPlayer = this.props.player;
+    return CheckersColors[checkersPlayer].border;
+  },
+
+  getFillColorCode: function() {
+    var checkersPlayer = this.props.player;
+    return CheckersColors[checkersPlayer].fill;
+  },
+
+  getDesign: function() {
+    var checkersLevel = this.props.level;
+    switch (checkersLevel) {
+      case CheckersLevels.MAN:
+        return (
+          <circle
+            cx="25"
+            cy="25"
+            stroke={this.getBorderColorCode()}
+            stroke-width="1"
+            r="20"
+            fill={this.getFillColorCode()}
+          />
+        );
+      case CheckersLevels.KING:
+        return [
+          <circle
+            cx="25"
+            cy="25"
+            stroke={this.getBorderColorCode()}
+            stroke-width="1"
+            r="23"
+            fill={this.getFillColorCode()}
+          />,
+          <circle
+            cx="25"
+            cy="25"
+            stroke={this.getBorderColorCode()}
+            stroke-width="1"
+            r="20"
+            fill={this.getFillColorCode()}
+          />,
+          <line
+            x1="20"
+            y1="30"
+            x2="15"
+            y2="19"
+            stroke={this.getBorderColorCode()}
+            stroke-width="1"
+          />,
+          <line
+            x1="25"
+            y1="30"
+            x2="25"
+            y2="15"
+            stroke={this.getBorderColorCode()}
+            stroke-width="1"
+          />,
+          <line
+            x1="30"
+            y1="30"
+            x2="35"
+            y2="19"
+            stroke={this.getBorderColorCode()}
+            stroke-width="1"
+          />
+        ];
+    }
+    console.error('Invalid level ' + checkersLevel);
+    return null;
+  },
+
+  render: function() {
+    var colorCode = this.getFillColorCode();
+    return (
+      <div className="checkersPiece">
+        <svg>
+          <circle
+            cx="25"
+            cy="25"
+            stroke={this.getBorderColorCode()}
+            stroke-width="1"
+            r="25"
+            fill={colorCode}
+          />
+          {this.getDesign()}
+        </svg>
+      </div>
+    );
+  }
+});
+
+if (Meteor.isClient) {
+  Meteor.startup(function() {
+    React.renderComponent(
+      <CheckersApp />,
+      document.getElementById('checkers_stage')
+    );
+  });
+}
+
 function getInitialBoardPieces() {
   var playerOnePositions = [
     [0, 0],
@@ -66,183 +385,19 @@ function getInitialBoardPieces() {
   return pieces;
 }
 
-var CheckersStage = React.createClass({
-  getInitialState: function() {
-    return {
+Meteor.methods({
+  createGame: function(name) {
+    return CheckersGames.insert({
+      name: name,
       pieces: getInitialBoardPieces()
-    };
-  },
-
-  render: function() {
-    var gameData = {
-      width: 8,
-      height: 8,
-      pieces: this.state.pieces
-    };
-    return (
-      <CheckersBoard
-        data={gameData}
-      />
-    );
+    });
   }
 });
 
-var CheckersBoard = React.createClass({
-  getPieceAtLocation: function(r, c) {
-    var row, pieceData;
-    var pieces = this.props.data.pieces;
-    if (row = pieces[r]) {
-      if (pieceData = row[c]) {
-        return (
-          <CheckersPiece
-            player={pieceData.player}
-            level={pieceData.level}
-          />
-        );
-      }
-    }
-    return null;
-  },
-
-  render: function() {
-    var data = this.props.data;
-    var width = data.width;
-    var height = data.height;
-    var rows = [];
-    var parity = true;
-    for (var i = 0; i < height; i++) {
-      var checkersCells = [];
-      for (var j = 0; j < width; j++) {
-        checkersCells.push(
-          <CheckersCell
-            key={'cell.' + i + '.' + j}
-            piece={this.getPieceAtLocation(i, j)}
-            parity={(parity = !parity)}
-          />
-        );
-      }
-      parity = !parity;
-      rows.push(
-        <CheckersRow
-          key={'row.' + i}
-          cells={checkersCells}
-        />
-      );
-    }
-    return (
-      <table className="checkersBoard">
-        <tbody>
-          {rows}
-        </tbody>
-      </table>
-    );
-  }
-});
-
-var CheckersRow = React.createClass({
-  render: function() {
-    return (
-      <tr className="checkersRow">
-        {this.props.cells}
-      </tr>
-    );
-  }
-});
-
-var CheckersCell = React.createClass({
-  render: function() {
-    var classNameSuffix = this.props.parity
-      ? "odd"
-      : "even";
-    return (
-      <td className={['checkersCell', classNameSuffix].join(' ')}>
-        {this.props.piece}
-      </td>
-    );
-  }
-});
-
-var CheckersPiece = React.createClass({
-  getBorderColorCode: function() {
-    var checkersPlayer = this.props.player;
-    return CheckersColors[checkersPlayer].border;
-  },
-
-  getFillColorCode: function() {
-    var checkersPlayer = this.props.player;
-    return CheckersColors[checkersPlayer].fill;
-  },
-
-  getDesign: function() {
-    var checkersLevel = this.props.level;
-    switch (checkersLevel) {
-      case CheckersLevels.MAN:
-        return null;
-      case CheckersLevels.KING:
-        return [
-          <line
-            x1="20"
-            y1="30"
-            x2="15"
-            y2="19"
-            stroke={this.getBorderColorCode()}
-            stroke-width="1"
-          />,
-          <line
-            x1="25"
-            y1="30"
-            x2="25"
-            y2="15"
-            stroke={this.getBorderColorCode()}
-            stroke-width="1"
-          />,
-          <line
-            x1="30"
-            y1="30"
-            x2="35"
-            y2="19"
-            stroke={this.getBorderColorCode()}
-            stroke-width="1"
-          />
-        ];
-    }
-    console.error('Invalid level ' + checkersLevel);
-    return null;
-  },
-
-  render: function() {
-    var colorCode = this.getFillColorCode();
-    return (
-      <div className="checkersPiece">
-        <svg>
-          <circle
-            cx="25"
-            cy="25"
-            stroke={this.getBorderColorCode()}
-            stroke-width="1"
-            r="25"
-            fill={colorCode}
-          />
-          <circle
-            cx="25"
-            cy="25"
-            stroke={this.getBorderColorCode()}
-            stroke-width="1"
-            r="20"
-            fill={this.getFillColorCode()}
-          />
-          {this.getDesign()}
-        </svg>
-      </div>
-    );
-  }
-});
-
-if (Meteor.isClient) {
+if (Meteor.isServer) {
   Meteor.startup(function() {
-    React.renderComponent(
-      <CheckersStage />,
-      document.getElementById('checkers_stage')
-    );
+    if (CheckersGames.find().count() === 0) {
+      Meteor.call('createGame', 'MyGame');
+    }
   });
 }
